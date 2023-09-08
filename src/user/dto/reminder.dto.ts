545a77ae5777cb2143b454b2d37efe9100862c82
KhadkaRipesh <khadkanripesh@gmail.com>
2023-09-08 15:31:14 +0545

    response reminder

---
+++ src/user/dto/reminder.dto.ts
@@ -0,0 +1,3 @@
+export class ResponseReminderDto {
+  giveAccess: string;
+}
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,17 @@
-import { Controller } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { ResponseReminderDto } from './dto/reminder.dto';
 
-@ApiTags('User')
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  //    To response reminder
+  @Post('respond-reminder/:id')
+  responseToReminder(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() payload: ResponseReminderDto,
+  ) {
+    return this.userService.responseToReminder(id, payload);
+  }
+}
