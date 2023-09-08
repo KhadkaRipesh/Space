43df933ab2b32cc7ce861a646018d91d07f038bd
KhadkaRipesh <khadkanripesh@gmail.com>
2023-09-08 10:55:25 +0545

    configuration

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfig } from './@config/typeorm.config';
+
+@Module({
+  imports: [
+    AuthModule,
+    UserModule,
+    TypeOrmModule.forRootAsync({ useFactory: () => TypeOrmConfig }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
