32b073db93c077159e3953ebb4bb812c2174d4c6
KhadkaRipesh <khadkanripesh@gmail.com>
2023-09-11 10:01:13 +0545

    update swagger

--- src/message/message.controller.ts
+++ src/message/message.controller.ts
@@ -13,8 +13,16 @@ import { GetUser } from 'src/@docoraters/getUser.decorater';
 import { User } from 'src/user/entities/user.entity';
 import { CreateMessageDto } from './dto/message.dto';
 import { UserService } from 'src/user/user.service';
-import { ApiBadRequestResponse, ApiBearerAuth, ApiCreatedResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags('Message')
 @Controller('message')
 export class MessageController {
   constructor(
@@ -25,11 +33,14 @@ export class MessageController {
   //-------------CREATE MESSAGE ROUTE -----------------
   @UseGuards(JwtAuthGuard)
   @Post()
-  @ApiBearerAuth("Auth")
-  @ApiOperation({summary: 'Create a Message'})
-  @ApiCreatedResponse({description: 'Sucessfully created a message', type: CreateMessageDto })
-  @ApiOkResponse({description: 'Sucessfully created a message'})
-  @ApiBadRequestResponse({description: 'Failed to create a Message'})
+  @ApiBearerAuth('Auth')
+  @ApiOperation({ summary: 'Create a Message' })
+  @ApiCreatedResponse({
+    description: 'Sucessfully created a message',
+    type: CreateMessageDto,
+  })
+  @ApiOkResponse({ description: 'Sucessfully created a message' })
+  @ApiBadRequestResponse({ description: 'Failed to create a Message' })
   async createMessage(
     @GetUser() user: User,
     @Body() payload: CreateMessageDto,
@@ -43,9 +54,9 @@ export class MessageController {
   @UseGuards(JwtAuthGuard)
   @Get(':id')
   @ApiBearerAuth('Auth')
-  @ApiOperation({summary: 'Sucessfully get a message'})
-  @ApiOkResponse({description: 'Sucessfully getting a message'})
-  @ApiBadRequestResponse({description: 'Failed to get a message'})
+  @ApiOperation({ summary: 'Sucessfully get a message' })
+  @ApiOkResponse({ description: 'Sucessfully getting a message' })
+  @ApiBadRequestResponse({ description: 'Failed to get a message' })
   async getMessage(
     @GetUser() user: User,
     @Param('id', new ParseUUIDPipe()) id: string,
